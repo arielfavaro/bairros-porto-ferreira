85f7a625b2e5d5688adb86a8a9d6856c50b1e346
Ariel Favaro <arieltainan@gmail.com>
2025-07-26 16:38:06 -0300

    Ajustes gerais, adicionado seletor de bairros

--- src/main.js
+++ src/main.js
@@ -4,10 +4,49 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { point as turfPoint, booleanPointInPolygon } from '@turf/turf';
 import Alpine from 'alpinejs';
+import poligonos from './data/poligonos.json'
 
 // window.Alpine = Alpine;
 
 Alpine.start();
+
+let geoLayer = null;
+
+const bairros = new Set();
+
+for (const feature of poligonos.features) {
+  bairros.add(feature.properties.localidade);
+}
+
+const seletorBairros = document.getElementById('bairros');
+
+for (const bairro of bairros) {
+  const option = document.createElement('option');
+  option.value = bairro;
+  option.textContent = bairro;
+  seletorBairros.appendChild(option);
+}
+
+const onChangeSeletorBairros = (event) => {
+  const bairroSelecionado = event.target.value;
+
+  // agora, para cada polígono, checar se o ponto está dentro do seu bounds
+  geoLayer.eachLayer(layer => {
+    const feature = layer.feature;
+    const nomeBairro = feature.properties.localidade;
+
+    if (nomeBairro === bairroSelecionado) {
+      // se o nome do bairro for igual ao selecionado, destacar
+      layer.setStyle({ color: 'red' });
+      map.flyToBounds(layer.getBounds(), {
+        padding: [150, 150],
+      });
+    } else {
+      // caso contrário, resetar o estilo
+      geoLayer.resetStyle(layer);
+    }
+  });
+}
 
 // inicializa o mapa
 const map = L.map('map').setView([-21.861884, -47.4787214], 12);
@@ -18,64 +57,59 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>'
 }).addTo(map);
 
-// carrega o GeoJSON
-fetch('/poligonos.geojson')
-  .then(res => {
-    if (!res.ok) throw new Error('Falha ao carregar GeoJSON');
-    return res.json();
-  })
-  .then(geojson => {
-    // adiciona ao mapa
-    const geoLayer = L.geoJSON(geojson, {
-      style: feature => ({
-        color: '#3388ff',
-        weight: 2,
-        fillOpacity: 0.3
-      }),
-      onEachFeature: (feature, layer) => {
-        // popup opcional com propriedades
-        // if (feature.properties && feature.properties.localidade) {
-        //   layer.bindPopup(feature.properties.localidade);
-        // }
-      }
-    }).addTo(map);
+// adiciona ao mapa
+geoLayer = L.geoJSON(poligonos, {
+  style: feature => ({
+    color: '#3388ff',
+    weight: 2,
+    fillOpacity: 0.3
+  }),
+  onEachFeature: (feature, layer) => {
+    // popup opcional com propriedades
+    // if (feature.properties && feature.properties.localidade) {
+    //   layer.bindPopup(feature.properties.localidade);
+    // }
+  }
+}).addTo(map);
 
-    // ajusta o mapa para o bounds do GeoJSON
-    map.fitBounds(geoLayer.getBounds());
+// ajusta o mapa para o bounds do GeoJSON
+map.fitBounds(geoLayer.getBounds());
 
-    // adiciona o listener de clique no mapa
-    map.on('click', e => {
-      const clickedLocalidades = [];
+// adiciona o listener de clique no mapa
+map.on('click', e => {
+  const clickedLocalidades = [];
 
-      // ponto GeoJSON para o Turf (lng, lat)
-      const pontoClicado = turfPoint([e.latlng.lng, e.latlng.lat]);
+  // ponto GeoJSON para o Turf (lng, lat)
+  const pontoClicado = turfPoint([e.latlng.lng, e.latlng.lat]);
 
-      // Resetar estilo de todos os polígonos
-      geoLayer.eachLayer(layer => {
-        geoLayer.resetStyle(layer);
-      });
+  // Resetar estilo de todos os polígonos
+  geoLayer.eachLayer(layer => {
+    geoLayer.resetStyle(layer);
+  });
 
-      // agora, para cada polígono, checar se o ponto está dentro do seu bounds
-      geoLayer.eachLayer(layer => {
-        const feature = layer.feature;
-        if (booleanPointInPolygon(pontoClicado, feature)) {
-          layer.setStyle({ color: 'red' });
-          const loc = feature.properties.localidade;
-          if (loc != null) clickedLocalidades.push(loc);
-        }
-      });
+  // reseta o seletor de bairros
+  seletorBairros.value = '';
 
-      // se encontrou algum, mostra popup
-      if (clickedLocalidades.length) {
-        L.popup({ maxWidth: 300 })
-          .setLatLng(e.latlng)
-          .setContent(
-            `<strong>Bairros:</strong><br><br>` +
-            clickedLocalidades.join('<br>')
-          )
-          .openOn(map);
-      }
-    });
+  // agora, para cada polígono, checar se o ponto está dentro do seu bounds
+  geoLayer.eachLayer(layer => {
+    const feature = layer.feature;
+    if (booleanPointInPolygon(pontoClicado, feature)) {
+      layer.setStyle({ color: 'red' });
+      const loc = feature.properties.localidade;
+      if (loc != null) clickedLocalidades.push(loc);
+    }
+  });
 
-  })
-  .catch(err => console.error(err));
+  // se encontrou algum, mostra popup
+  if (clickedLocalidades.length) {
+    L.popup({ maxWidth: 300 })
+      .setLatLng(e.latlng)
+      .setContent(
+        `<strong>Bairros:</strong><br><br>` +
+        clickedLocalidades.join('<br>')
+      )
+      .openOn(map);
+  }
+});
+
+seletorBairros.addEventListener('change', onChangeSeletorBairros);
